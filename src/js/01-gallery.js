d6b0c2ca63dbbd8f801f72a172582d707a5a5f2d
VovaMyb <rimidalvatopayl@gmail.com>
2023-05-23 21:20:19 +0300

    1-2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const listEls = document.querySelector('.gallery');
+
+const newItemsOfListEls = galleryItems
+  .map(obj => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${obj.original}">
+      <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+   </a>
+   </a>
+</li>`;
+  })
+  .join('');
+
+listEls.insertAdjacentHTML('beforeend', newItemsOfListEls);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: '.gallery__image',
+  captionType: 'inner',
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (timeupdate) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(timeupdate.seconds)
+    );
+  }, 1000)
+);
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
